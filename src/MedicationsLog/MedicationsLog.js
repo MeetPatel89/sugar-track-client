6aa1977ea915ce47e592eb2991b532163c6c54f5
Meet Patel <meetaiims@yahoo.com>
2020-11-24 18:41:37 -0500

    add required attr to form elements

--- src/MedicationsLog/MedicationsLog.js
+++ src/MedicationsLog/MedicationsLog.js
@@ -7,16 +7,16 @@ export default class MedicationsLog extends Component {
         <h2>Enter the medication regimens</h2>
         <form>
           <label htmlFor="pill">Medication:</label>
-          <input type="text" id="pill" name="pill" placeholder="E.g. Insulin or Metformin"/>
+          <input type="text" id="pill" name="pill" placeholder="E.g. Insulin or Metformin" required/>
           <br/>
           <br/>
           <div>
               <label htmlFor="date">Date:</label>
-              <input type="date" name='date' id='date' />
+              <input type="date" name='date' id='date' required/>
               <br/>
               <br/>
               <label htmlFor="time">Time:</label>
-              <input type="time" id="time" name="time"/>
+              <input type="time" id="time" name="time" required/>
             </div>
             <br/>
           <button type="submit">Add</button>
