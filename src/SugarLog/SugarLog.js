05e7491920eec16add57402e6b83803472323ad2
Meet Patel <meetaiims@yahoo.com>
2020-11-24 09:58:52 -0500

    validate no duplicate glucose logs

--- src/SugarLog/SugarLog.js
+++ src/SugarLog/SugarLog.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 export default class SugarLog extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ''
+      message: '',
+      error: ''
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
+    const date_time = moment(`${this.state.date}T${this.state.time}`).toISOString()
     const newGlucoseLog = {
-      ...this.state
+      user_id: this.props.id,
+      date_time,
+      glucose: this.state.glucose
     };
-    delete newGlucoseLog['message'];
+    
+    fetch('http://localhost:8000/glucose_logs')
+    .then(res => res.json())
+    .then(glucoseLogs => {
+      console.log(newGlucoseLog);
+      const duplicateGlucoseLog = glucoseLogs.find(glucoseLog => glucoseLog.date_time === newGlucoseLog.date_time)
+      console.log(duplicateGlucoseLog);
+      if (duplicateGlucoseLog) {
+        this.setState({
+          error: 'You have already logged a glucose value for the selected date and time',
+          message: ''
+        })
+      } else {
+        fetch('http://localhost:8000/glucose_logs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newGlucoseLog)
+    })
+    .then(newGlucoseLog => newGlucoseLog.json())
+    .then(newGlucoseLog => {
+      console.log(newGlucoseLog);
+      
+      this.setState({
+        message: 'You have successfully logged a glucose value',
+        error: ''
+      })
+    })
+      }
+      
+    })
+    
+    /*
     fetch('http://localhost:8000/glucose_logs', {
       method: 'POST',
       headers: {
@@ -24,10 +62,18 @@ export default class SugarLog extends Component {
     .then(newGlucoseLog => newGlucoseLog.json())
     .then(newGlucoseLog => {
       console.log(newGlucoseLog);
+      
       this.setState({
         message: 'You have successfully logged a glucose value'
       })
     })
+   */
+    
+   
+   console.log(moment(`${this.state.date}T${this.state.time}`).toString())
+   console.log(moment(`${this.state.date}T${this.state.time}`).toISOString())
+   
+
     
   }
 
@@ -36,7 +82,7 @@ export default class SugarLog extends Component {
     const value = e.target.value;
     this.setState({
       [name]: value,
-      user_id: this.props.id
+      
     })
   }
 
@@ -62,6 +108,7 @@ export default class SugarLog extends Component {
             <br/>
             <button type='submit'>Save</button>
             {this.state.message}
+            {this.state.error}
         </form>
         </section>
         )
