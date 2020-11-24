4e2f589418242d048a50066784f2e875b9775935
Meet Patel <meetaiims@yahoo.com>
2020-11-24 18:44:07 -0500

    initialiaze state using constructor

--- src/MedicationsLog/MedicationsLog.js
+++ src/MedicationsLog/MedicationsLog.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class MedicationsLog extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: '',
+        error: ''
+      }
+    }
+
     render() {
         return (
             <section>
