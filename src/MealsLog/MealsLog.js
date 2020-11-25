12e8d3571c8a05ffb984f6c8caf1f6afe17228ce
Meet Patel <meetaiims@yahoo.com>
2020-11-24 21:53:36 -0500

    add handleSubmit method and console logging to check it works

--- src/MealsLog/MealsLog.js
+++ src/MealsLog/MealsLog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 export default class MealsLog extends Component {
   constructor(props) {
@@ -17,13 +18,26 @@ export default class MealsLog extends Component {
     })
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const meals = this.state.meals;
+    const user_id = this.props.id;
+    const date_time_moment = moment(`${this.state.date} ${this.state.time}`, 'YYYY-MM-DD HH:mm');
+    const date_time = date_time_moment.toISOString();
+    console.log({
+      meals,
+      user_id,
+      date_time
+    })
+  }
+
     render() {
         return (
             <section>
         <header>
             <h3>Enter Meal Regimens</h3>
         </header>
-        <form className='signup-form'>
+        <form className="meals_log" onSubmit={this.handleSubmit}>
             <div>
               <label htmlFor="meal">Meal:</label>
               <input placeholder='Egg Sandwich' type="text" name="meals" id="meals" onChange={this.handleChange} required/>
