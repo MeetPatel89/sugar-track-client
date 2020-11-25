e332b1f849c1ad23e86e398e5f0d2c5c8a48f25e
Meet Patel <meetaiims@yahoo.com>
2020-11-24 21:39:34 -0500

    initialize state using constructor

--- src/MealsLog/MealsLog.js
+++ src/MealsLog/MealsLog.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class MealsLog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+      message: ''
+    }
+  }
+
     render() {
         return (
             <section>
