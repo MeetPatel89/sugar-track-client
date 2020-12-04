0e406cc60e740bb50d533a290a240dee7226fcd7
Meet Patel <meetaiims@yahoo.com>
2020-12-04 10:06:20 -0600

    css to remove list style from display list items

--- src/Logdisplay/Logdisplay.js
+++ src/Logdisplay/Logdisplay.js
@@ -153,11 +153,11 @@ export default class LogDisplay extends Component {
             renderLogs = this.state.filteredLogs.map((log, i) => {
                 const date_time = moment(log.date_time).format('HH:mm');
                 if (log.glucose) {
-                return <li className="log-display-list" key={`${i}`}>Your blood sugar level was {log.glucose} at {date_time}</li>
+                return <li className="log-display-list-item" key={`${i}`}>Your blood sugar level was {log.glucose} at {date_time}</li>
                 } else if (log.meds) {
-                return <li className="log-display-list" key={i}>You took {log.meds} medication(s) at {date_time} </li>
+                return <li className="log-display-list-item" key={i}>You took {log.meds} medication(s) at {date_time} </li>
                 } else {
-                return <li className="log-display-list" key={i}>You ate {log.meals} at {date_time} </li>
+                return <li className="log-display-list-item" key={i}>You ate {log.meals} at {date_time} </li>
                 }
             })
         } else {
@@ -200,7 +200,10 @@ export default class LogDisplay extends Component {
                 
                 </div>
                 <button type="submit">Display logs</button>
+                <ul className="log-display-list">
                 {renderLogs}
+                </ul>
+                
             </form>
            
         
