393dcd9c6f13ac11567a725431d6dd390ddfbe4b
Meet Patel <meetaiims@yahoo.com>
2020-12-04 08:21:29 -0600

    smoke test logbook component

---
+++ src/LogBook/LogBook.test.js
@@ -0,0 +1,23 @@
+// make React available
+import React from 'react';
+
+// make the ReactDOM available necessary for rendering the component
+import ReactDOM from 'react-dom';
+
+// make the LogBook component available
+import LogBook from './LogBook';
+
+// import BrowserRouter to wrap around App
+import { BrowserRouter } from 'react-router-dom';
+
+// this is the test case
+it('renders component without crashing', () => {
+    // first create a DOM element to render the component into
+    const div = document.createElement('div');
+
+    // render the component, this is the actual test, if something is wrong it will fail here
+    ReactDOM.render(<BrowserRouter><LogBook/></BrowserRouter>, div);
+
+    // clean up code
+    ReactDOM.unmountComponentAtNode(div);
+});
